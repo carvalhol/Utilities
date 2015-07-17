8f06bef079dcb653b25b117e8313c908a0e3e202
carvalhol <luciano.de-carvalho@ecp.fr>
2015-07-17 18:38:10 +0200

    bash vai e limpa

---
+++ AutoTest/limpa.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/carvalhol/Projects/Utilities/AutoTest/
+echo ""
+rm -r COMP* STRONG* WEAK*
+# INSTRUCIONS TO CREATE A BASH FILE
+# create a file like this "FILETEST.sh"
+# change the mode of the file, so you can execute this: "chmod u+x FILETEST.sh"
+# it can be useful to make things you do every day automatically by executing this file
+# more about it on http://tldp.org/LDP/Bash-Beginners-Guide/html/
